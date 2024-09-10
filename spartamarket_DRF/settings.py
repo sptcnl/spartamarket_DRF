6af7a6777e9a643cfb5d0d06795f0e47a645109c
nyong <sptcnl@gmail.com>
2024-09-10 13:50:58 +0900

    update: added django_extensions, django_seed in settings.py

--- spartamarket_DRF/settings.py
+++ spartamarket_DRF/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
+    'django_seed',
     'accounts',
     'products',
 ]
